7397f3acbc078fb0604fffae4497660381dfa20d
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-04-22 13:41:25 +0530

    conecting mongo db cluster

--- dist/app.js
+++ dist/app.js
@@ -1,20 +1,25 @@
 "use strict";
+//
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-//
+const dotenv_1 = require("dotenv");
+//dotenv configs
+(0, dotenv_1.config)();
+// import { log } from "connect";
 const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
 //create express app
 const app = (0, express_1.default)();
-mongoose_1.default.connect("mongodb+srv://root:1234@testcluster.1sdqczd.mongodb.net/?retryWrites=true&w=majority").then(() => {
-    console.log("Database Connected !");
-    app.listen(4000, () => {
-        console.log("Hello their ! ");
+mongoose_1.default.connect(process.env.MONGO_DB_URL)
+    .then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log("Database Connected !");
     });
-}).catch((error) => {
-    console.log("Failed to connect MongoDB " + error);
+})
+    .catch(() => {
+    console.log("Failed to connect MongoDB ");
 });
 // //TODO : me port eke server eka up wenna kiyanwa
 // app.listen(4000, () => {
--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,27 @@
 //
+
+import { config } from "dotenv";
+//dotenv configs
+config();
+
+// import { log } from "connect";
 import express from "express";
 import db from "mongoose";
 
 //create express app
 const app = express();
 
-
-db.connect(
-  "mongodb+srv://root:1234@testcluster.1sdqczd.mongodb.net/?retryWrites=true&w=majority"
-).then(() => {
-    console.log("Database Connected !");
-    app.listen(4000, () => {
-      console.log("Hello their ! ");
+db.connect(process.env.MONGO_DB_URL!)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("Database Connected !");
     });
+  })
+  .catch(() => {
+    console.log("Failed to connect MongoDB ");
+  });
 
-}).catch((error) => {
-    console.log("Failed to connect MongoDB "+ error);
-});
-
-
-// //TODO : me port eke server eka up wenna kiyanwa
+//TODO : me port eke server eka up wenna kiyanwa
 // app.listen(4000, () => {
 //     console.log("Hello their ! ");
 // });
