88cbb1c8e7d496809623a4defe007a93f85e7a25
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-04-23 12:28:30 +0530

    introducing postcontroller with createpost method

--- src/controllers/PostController.ts
+++ src/controllers/PostController.ts
@@ -1,5 +1,12 @@
 import { Request, RequestHandler, Response } from "express";
 
-export default class UserController { 
-    createPost 
+export default class PostController {
+  createPost: RequestHandler = async (
+    req: Request,
+    res: Response
+  ): Promise<Response> => {
+    return res;
+  };
+    
+    
 }
